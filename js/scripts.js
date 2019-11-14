41b3f55886918691320b6c8b3803d4eab3e6d30a
Elizabeth Van Hook and Matthew Swart <elizabeth@>
2019-11-13 20:47:58 -0800

    add color changes

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,22 @@ $(document).ready(function() {
   $("#penguin-click").click(function() {
     $ ("#penguin-slide").slideToggle();
   });
+  $("button#black").click(function(){
+    $("body").removeClass();
+    $("body").addClass("background-color");
+    $(".show").toggle();
+    $(".hide").toggle();
+  });
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("original-color");
+    $(".show").toggle();
+    $(".hide").toggle();
+  });
+  $("p").click(function() {
+    $("p").addClass("highlight-box");
+  });
+  $("button#hi").click(function() {
+    $("ul").after("<li>Hi there</li>");
+  });
 });
